c4a05f3b3b4df45f1f18f29dd0c97f089deccbb1
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-10-09 06:41:52 -0300

    deploy

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = axios.create({
     // Endereço da API - localhost
-    baseURL: "http://localhost:3333"
+    //baseURL: "http://localhost:3333"
     // Disponível pelo Render
-    // baseURL: "https://mynotes-xjhu.onrender.com"
+    baseURL: "https://advicehealth-api.onrender.com"
 })
